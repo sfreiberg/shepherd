4793e1edafb8836a89c8993afdcc4c141dbb671c
Sam Freiberg <sam@netserious.com>
2013-04-14 13:29:09 -0700

    Fix javascript.

--- examples/user.js
+++ examples/user.js
@@ -4,7 +4,7 @@ user(
 		homeDir: "/home/user1",
 		shell: "/bin/bash",
 		uid: 500,
-		gid: 500
+		gid: 500,
 		password: "$6$somegiantcrazyhashthathasbeencryptedorsomething",
 		groups: ["sudo"]
 	}
