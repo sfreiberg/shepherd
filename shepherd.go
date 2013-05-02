ddc0f9be516e9609c728c4a06904db4790efabac
Sam Freiberg <sam@netserious.com>
2013-05-01 21:32:39 -0700

    Add subcommand to print facts.

--- shepherd.go
+++ shepherd.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"github.com/sfreiberg/facts"
+
+	"fmt"
 	"os"
 )
 
@@ -9,10 +12,20 @@ func main() {
 		switch os.Args[1] {
 		case "server":
 			// run server
+		case "facts":
+			ShowFacts()
 		case "client":
 			RunClient()
 		default:
 			RunStandalone()
 		}
 	}
+}
+
+func ShowFacts() {
+	allFacts, err := facts.FindFacts().ToPrettyJson()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", allFacts)
 }
